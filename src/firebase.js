42ae13b855d0f049c9a4e61f3fdfad08360a50ee
Erik Maan <87411830+Failfan13@users.noreply.github.com>
2024-02-13 22:23:46 +0100

    Initial setup

---
+++ src/firebase.js
@@ -0,0 +1,22 @@
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+// import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
+// import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-store.js";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCqvqttbuHrwbpzRaFOFeTckzVtZFXJMSE",
+  authDomain: "personalbranding-1-22407.firebaseapp.com",
+  databaseURL: "https://personalbranding-1-22407-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "personalbranding-1-22407",
+  storageBucket: "personalbranding-1-22407.appspot.com",
+  messagingSenderId: "851525119268",
+  appId: "1:851525119268:web:f64992339a1f8d198897b1"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
