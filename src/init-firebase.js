89a4435e21b477be5f0e0ee74ec8b06f7962efe0
Erik Maan <erikmaan02@gmail.com>
2024-02-27 12:25:04 +0100

    fix firebase initialised

---
+++ src/init-firebase.js
@@ -0,0 +1,13 @@
+
+// // Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCqvqttbuHrwbpzRaFOFeTckzVtZFXJMSE",
+  authDomain: "personalbranding-1-22407.firebaseapp.com",
+  databaseURL: "https://personalbranding-1-22407-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "personalbranding-1-22407",
+  storageBucket: "personalbranding-1-22407.appspot.com",
+  messagingSenderId: "851525119268",
+  appId: "1:851525119268:web:f64992339a1f8d198897b1"
+};
+
+firebase.initializeApp(firebaseConfig);
