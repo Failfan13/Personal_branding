910adba34cb5f4e90841a2fcb08eb530a7c97986
Erik Maan <87411830+Failfan13@users.noreply.github.com>
2024-03-10 16:55:02 +0100

    Fix navbar and language change

--- public/scripts/language.js
+++ public/scripts/language.js
@@ -7,16 +7,15 @@ LoadJson().then(data => {
 
   // ------------------------------ set language ------------------------------
   // set document language
-  SetNewLang = (CurrLang) => {
+  SetNewLang = (CurrLang) => { // if first time loaded
     if (! document.documentElement.lang && ! localStorage.getItem('personalisedLanguage')) {
       document.documentElement.lang = `${CurrLang}`;
       return CurrLang.toUpperCase().slice(0 , 2);
     }
-    else if (! localStorage.getItem('personalisedLanguage')) {
-    
+    else if (! localStorage.getItem('personalisedLanguage')) { // If not set language before use browser language
       return document.documentElement.lang.toLowerCase().slice(0 , 2);
     }
-    else {
+    else { // If user set language before
       document.documentElement.lang = localStorage.getItem('personalisedLanguage').toLowerCase();
       return localStorage.getItem('personalisedLanguage').toUpperCase().slice(0 , 2);
     }
@@ -27,31 +26,35 @@ LoadJson().then(data => {
   // --------------------------- change doc language --------------------------
   const spans = document.getElementsByTagName('span');
 
+  setTimeout(() => {
   // loop through spans
-  for (let i = 0; i < spans.length; i++) {
-    // if span has data-txt attribute
-    if (spans[i].dataset.txt) {
-      // split data-txt into array
-      let words = spans[i].dataset.txt.split(' ');
-      // loop through array
-      var newLine = '';
-
-      for (let j = 0; j < words.length; j++) {
-        // Get right phrase from json
-        var request = CurrLang + '.' + words[j];
-        var EvalFunc= new Function('data', 'return data.'+request);
-
-        if (newLine != '') {
-          newLine += ' ';
+    for (let i = 0; i < spans.length; i++) {
+      // if span has data-txt attribute
+      if (spans[i].dataset.txt) {
+        // split data-txt into array
+        let words = spans[i].dataset.txt.split(' ');
         
-        }
-        newLine += EvalFunc(data);
-      }
+        var newLine = '';
+        // loop through array
+        for (let j = 0; j < words.length; j++) {
+          // Get right phrase from json
+          var request = CurrLang + '.' + words[j];
+          var EvalFunc = null;
+          EvalFunc = new Function('data', 'return data.'+request);
+          
 
-      // Set innerHTML to right phrase
-      spans[i].innerHTML = newLine;
+          if (newLine != '') {
+            newLine += ' ';
+          
+          }
+          newLine += EvalFunc(data);
+        }
+
+        // Set innerHTML to right phrase
+        spans[i].innerHTML = newLine;
+      }
     }
-  }
+  }, 1000);
 });
 
 
--- public/scripts/script.js
+++ public/scripts/script.js
@@ -3,15 +3,48 @@ setTimeout(() => {
     const lang = document.documentElement.lang.toLowerCase().slice(0 , 2);
     document.getElementById('dropdown-img').attributes[1].value = `images/${lang}.png`;
 }, 50);
-// const lang = document.documentElement.lang.toLowerCase().slice(0 , 2);
-// console.log(document.getElementById('dropdown-img').attributes[1].value = `images/${lang}.png`);
 
 // -------- Clickable dropdown items
 var langBtns = document.getElementsByClassName('language-item');
 for (var i = 0; i < langBtns.length; i++) {
     langBtns[i].addEventListener('click', function(e){
         e.preventDefault();
-        localStorage.setItem('personalisedLanguage', e.target.parentElement.classList[2]);
+        newLang = e.target.parentElement.classList[2];
+        var timeout = 0;
+        while (newLang == undefined && timeout <= 5) {
+            setTimeout(() => {
+                newLang = e.target.parentElement.classList[2];
+            }, 1000);
+            
+            if (timeout == 5) {
+                newLang = "EN";
+                break;
+            }
+            timeout++;
+        }
+        localStorage.setItem('personalisedLanguage', newLang);
         window.location.reload();
     });
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    // Get all "navbar-burger" elements
+    const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+  
+    // Add a click event on each of them
+    $navbarBurgers.forEach( el => {
+      el.addEventListener('click', () => {
+  
+        // Get the target from the "data-target" attribute
+        const target = el.dataset.target;
+        const $target = document.getElementById(target);
+  
+        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+        el.classList.toggle('is-active');
+        $target.classList.toggle('is-active');
+  
+      });
+    });
+  
+  });
